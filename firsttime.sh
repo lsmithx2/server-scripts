c5f3eb84243dd42eb85b9200c05a58bfa480dedb
LSmithx2 <42276854+lsmithx2@users.noreply.github.com>
2022-12-16 13:00:55 +0000

    Update firsttime.sh

--- firsttime.sh
+++ firsttime.sh
@@ -1,1 +1,10 @@
+#!/bin/bash
 
+echo Making root user accessible from the outside
+sudo sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config
+
+echo Disabling IPTables rules which interfer with UFW
+sudo iptables -P INPUT ACCEPT
+sudo iptables -P OUTPUT ACCEPT
+sudo iptables -P FORWARD ACCEPT
+sudo iptables -F
